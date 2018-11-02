edd1ec91275191e928f4d07ae478d7d917545bf4
_lukas <lukas.folle@fau.de>
2018-11-02 13:38:56 +0100

    Update generate.py

--- network/generate.py
+++ network/generate.py
@@ -55,3 +55,6 @@ class GenerativeNetwork:
             pattern = self.corpus[seed:seed + self.sentence_length]
 
         return pattern
+    
+gn = GenerativeNetwork(corpus_path="../sonnets.txt", model_path="/content/shakespeare-LSTM/network/model.yaml", weights_path="/content/shakespeare-LSTM/network/weights-03-1.999.hdf5")
+gn.generate(gn.make_seed())
